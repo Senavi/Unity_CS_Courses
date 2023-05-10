cd433dc42eeb8920b5a982e4c4074bf0c6d2aa7d
Nazar <sergeyko.nazar@gmail.com>
2023-05-10 23:02:06 +0300

    Added movement and Rigidbody changes
    Added movement to the Worked with Rigidbody and force to control the rocket. Movement by Vector 3.

---
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+Rigidbody rb;
+[SerializeField] private float Thread = 10f;
+[SerializeField] private float RotationThread = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ProcessThrust();
+ProcessRotate();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+rb.AddRelativeForce(Vector3.up*Thread*Time.deltaTime);
+        }
+    }
+
+    void ProcessRotate()
+    {
+       if (Input.GetKey(KeyCode.D))
+        {
+       ApplyRotation(RotationThread);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(-RotationThread);
+        }
+    }
+
+
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(-Vector3.forward*rotationThisFrame*Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
